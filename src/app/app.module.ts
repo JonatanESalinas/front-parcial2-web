a97724b1838db58d597de6944464261ba842915f
JonatanESalinas <A01731815@itesm.mx>
2021-10-30 19:37:23 -0500

    Components were added #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ConfiguracionComponent } from './configuracion/configuracion.component';
+import { CalienteComponent } from './caliente/caliente.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ConfiguracionComponent,
+    CalienteComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
